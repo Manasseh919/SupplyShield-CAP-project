00e1dfa5179c5eb7e5c01ac3509f35bfaed4d787
Manasseh Ameyow <ameyow01@ads.uni-passau.de>
2026-07-30 16:30:06 +0000

    mocked users

---
+++ srv/auth/mock-users.js
@@ -0,0 +1,32 @@
+module.exports = {
+  users: [
+    {
+      username: 'inventory-planner',
+      roles: ['InventoryPlanner']
+    },
+    {
+      username: 'procurement-specialist',
+      roles: ['ProcurementSpecialist']
+    },
+    {
+      username: 'engineering-approver',
+      roles: ['EngineeringApprover']
+    },
+    {
+      username: 'quality-approver',
+      roles: ['QualityApprover']
+    },
+    {
+      username: 'finance-approver',
+      roles: ['FinanceApprover']
+    },
+    {
+      username: 'supply-chain-manager',
+      roles: ['SupplyChainManager']
+    },
+    {
+      username: 'administrator',
+      roles: ['Administrator']
+    }
+  ]
+};
